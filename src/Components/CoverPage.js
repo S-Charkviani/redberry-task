25a094aaecfa954274b97538b6c92178fcb5a814
Salome Charkviani <salo.charkviani@gmail.com>
2022-08-28 17:46:31 +0400

    Landing Visual

--- src/Components/CoverPage.js
+++ src/Components/CoverPage.js
@@ -1,14 +1,24 @@
 import React from "react";
 import classes from "./CoverPage.module.css";
-import WorkerInfo from "./WorkerInfo"
 
 const CoverPage = () => {
   return (
     <div className={classes.mainPage}>
+      {/* Redberry Logo */}
       <div className={classes.logo02}></div>
-      <div className={classes.cover}></div>
-      <button className={classes.button1}> ჩანაწერის დამატება</button>
-      <button className={classes.button2}>ჩანაწერების სია</button>
+
+      {/* vector pic and round logo */}
+      <div className={classes.frame}>
+        <div className={classes.cover}></div>
+        <div className={classes.logo01}></div>
+      </div>
+
+      <button className={classes.button1}>
+        <p>ჩანაწერის დამატება</p>
+      </button>
+      <button className={classes.button2}>
+        <p >ჩანაწერების სია </p>
+      </button>
     </div>
   );
 };
