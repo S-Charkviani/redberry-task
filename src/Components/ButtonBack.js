cd039e6ec079631ee2fa7a128ae48158cbb3a2a5
Salome Charkviani <salo.charkviani@gmail.com>
2022-09-05 21:06:26 +0400

    Back button

--- src/Components/ButtonBack.js
+++ src/Components/ButtonBack.js
@@ -1,26 +1,17 @@
-import React from 'react'
-import { useHistory } from 'react-router-dom'
-import classes from "./ButtonBack.module.css"
+import React from "react";
+import { useHistory } from "react-router-dom";
+import classes from "./ButtonBack.module.css";
 
-const ButtonBack = (page, setPage) => {
-const history=useHistory();
-
+const ButtonBack = () => {
+  const history = useHistory();
 
   return (
     <div className={classes.arrowButton}>
-      <button
-            onClick={() => {
-                if(page===1){
-                  setPage(page - 1);
-                }if(page===0){
-                history.goBack();
-                }
-              }}
-            >
-              <div className={classes.arrow}></div>
-            </button>
+      <button onClick={history.goBack}>
+        <div className={classes.arrow}></div>
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default ButtonBack
+export default ButtonBack;
